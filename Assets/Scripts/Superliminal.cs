a9f03dd12262e7eac977c32463ef14518c513f8a
yoonday <yoon2dev@gmail.com>
2024-11-04 19:29:39 +0900

    fix: Modify height adjustment

--- Assets/Scripts/Superliminal.cs
+++ Assets/Scripts/Superliminal.cs
@@ -99,10 +99,10 @@ public class Superliminal : MonoBehaviour
             target.localScale = targetScale * originalScale;
 
             // 중앙을 기준으로 커지므로, 크기 변화에 따라 높이를 보정
-            float heightAdjustment = (target.localScale.y * originalScale - originalScale) / 2;
+            float heightAdjustment = target.localScale.y / 2;
 
             RaycastHit floorHit;
-            if (Physics.Raycast(target.position + Vector3.up * 0.1f, Vector3.down, out floorHit, Mathf.Infinity, groundMask))
+            if (Physics.Raycast(target.position, Vector3.down, out floorHit, heightAdjustment + 0.1f, groundMask))
             {
                 target.position = new Vector3(target.position.x, floorHit.point.y + heightAdjustment + offsetFactor, target.position.z);
             }
