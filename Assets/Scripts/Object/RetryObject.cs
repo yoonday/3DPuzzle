05ffc89bfea613cb462b49912ddfe09be8f98aea
Seo-Woongbi <suwoongby@naver.com>
2024-11-07 13:01:37 +0900

    Update RetryObject
    아이템이 떨어졌을때 아이템만 자기 위치로 돌아가도록 만들어놓았다.

--- Assets/Scripts/Object/RetryObject.cs
+++ Assets/Scripts/Object/RetryObject.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RetryObject : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Item")  || collision.gameObject.CompareTag("Player")  )
+        Object[] objects = GameObject.FindObjectsOfType<Object>();
+
+        if (collision.gameObject.CompareTag("Player"))
         {
             DataManager.Instance.LoadCheckPoint();
+        }
+
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            foreach (var obj in objects)
+            {
+                obj.Initialize();
+            }
         }
     }
 }
